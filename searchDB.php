1005ce400e74c7f6f1b1a9555586f9e6079400b5
Liam Sullivan <60324016+liamSully43@users.noreply.github.com>
2021-01-24 17:28:38 +0000

    Styled search results & drinks page
     - Removed the default placeholder results and put actual results in its place
     - Saved/displayed the search term & filter tags when the page reloads with the results
     - Minor styling adjustments to the search button
     - Added the search feature to the drinks page & removed the ability to search for venues on /drinks
     - Styled/designed the drinks page

--- searchDB.php
+++ searchDB.php
@@ -32,16 +32,21 @@ function search($searchTerm) {
     $ale = (isset($_POST["ale"])) ? "ale" : "x";
     $wine = (isset($_POST["wine"])) ? "wine" : "x";
     $spirits = (isset($_POST["spirits"])) ? "spirits" : "x";
-    $venues = (isset($_POST["venues"])) ? "venues" : "x";
+    $venues = (isset($_POST["venues"])) ? "venue" : "x";
 
     // if no tags are selected then enable all types
-    if($cider === "x" && $lager === "x" && $ale === "x" && $wine === "x" && $spirits === "x" && $venues) {
+    if($cider === "x" && $lager === "x" && $ale === "x" && $wine === "x" && $spirits === "x" && $venues === "x") {
         $cider = "cider";
         $lager = "lager";
         $ale = "ale";
         $wine = "wine";
         $spirits = "spirits";
-        $venues = "venues";
+        $venues = "venue";
+    }
+    
+    // exclude the venues filter if the search function is called from the drinks page
+    if($_SERVER["REQUEST_URI"] === "/drinks") {
+        $venues = "";
     }
     
     $client = new MongoDB\Driver\Manager("mongodb+srv://$username:$password@cluster0.4fyfr.mongodb.net/$database?retryWrites=true&w=majority");
@@ -61,38 +66,11 @@ function search($searchTerm) {
 
     $query = new MongoDB\Driver\Query($filter);
     $cursor = $client->executeQuery("DiscoverDrink.items", $query);
-    // echo results
+    $results = [];
     foreach($cursor as $doc) {
-        echo $doc->type;
-        echo $doc->name;
-        echo $doc->percentage;
-        echo $doc->description;
-        $img = $doc->img;
-        echo "<img width='60' height='auto' src='$img' />";
+        array_push($results, $doc);
     }
 
-
-    // default/placeholder code for returning results
-    class Drinks {
-        public $name;
-        public $percentage;
-
-        function setName($name) {
-            $this->name = $name;
-        }
-
-        function setPercentage($per) {
-            $this->percentage = $per;
-        }
-    }
-
-    $one = new Drinks();
-    $two = new Drinks();
-    $one->setName("water");
-    $one->setPercentage("0%");
-    $two->setName("beer");
-    $two->setPercentage("4%");
-
-    return array($one, $two);
+    return $results;
 }   
 ?>
--- views/components/searchbar.php
+++ views/components/searchbar.php
@@ -1,16 +1,26 @@
 <?php
+    // get the directory the user is visiting to direct the user to the correct page when submitting the form
     if($_SERVER['REQUEST_URI'] === "/drinks") {
         $route = "/drinks";
     }
     else {
         $route = "/search";
     }
+    // save/show the search query & tags of a previous search
+    $searchTerm = (isset($_POST["search"])) ? $_POST["search"] : "";
+    $cider = (isset($_POST["cider"])) ? true : false;
+    $lager = (isset($_POST["lager"])) ? true : false;
+    $ale = (isset($_POST["ale"])) ? true : false;
+    $wine = (isset($_POST["wine"])) ? true : false;
+    $spirits = (isset($_POST["spirits"])) ? true : false;
+    $venues = (isset($_POST["venues"])) ? true : false;
 ?>
 <head>
     <link href="../styles.css" rel="stylesheet" type="text/css" />
     <link href="../CSS/searchbar.css" rel="stylesheet" type="text/css" />
 </head>
 <?php
+    // add the background & tags if the searchbar component is used on /search or /drinks
     if($_SERVER['REQUEST_URI'] === "/search" || $_SERVER['REQUEST_URI'] === "/drinks") {
         $background = "card";
     }
@@ -19,57 +29,64 @@
     }
 ?>
 <form method="POST" action="<?php echo $route ?>" class="<?php echo $background ?>">
-    <input type="text" name="search" placeholder="Search" />
-    <button class="dark" type="submit">Search</button>
+    <input type="text" name="search" placeholder="Search" value="<?php echo $searchTerm ?>" />
+    <button class="dark" type="submit">
+        <p>Search</p>
+        <span class="bg"></span>
+    </button>
     <div class="tag-container">
-        <label class="tags cider">
+        <label class="tags cider <?php if($cider) echo "active" ?>"> <!-- adds the active class if selected on the last search -->
             Cider
-            <input id="cider" type="checkbox" name="cider" value="checked" />
+            <input id="cider" type="checkbox" name="cider" value="checked" <?php if($cider) echo "checked" ?> /> <!-- checks the checkbox of selected on the last search -->
             <span class="custom-checkbox">&chi;</span>
         </label>
-        <label class="tags lager">
+        <label class="tags lager <?php if($lager) echo "active" ?>">
             Lager
-            <input id="lager" type="checkbox"name="lager" value="checked" />
+            <input id="lager" type="checkbox"name="lager" value="checked" <?php if($lager) echo "checked" ?> />
             <span class="custom-checkbox">&chi;</span>
         </label>
-        <label class="tags ale">
+        <label class="tags ale <?php if($ale) echo "active" ?>">
             Ale
-            <input id="ale" type="checkbox"name="ale" value="checked" />
+            <input id="ale" type="checkbox"name="ale" value="checked" <?php if($ale) echo "checked" ?> />
             <span class="custom-checkbox">&chi;</span>
         </label>
-        <label class="tags wine">
+        <label class="tags wine <?php if($wine) echo "active" ?>">
             Wine
-            <input id="wine" type="checkbox"name="wine" value="checked" />
+            <input id="wine" type="checkbox"name="wine" value="checked" <?php if($wine) echo "checked" ?> />
             <span class="custom-checkbox">&chi;</span>
         </label>
-        <label class="tags spirits">
+        <label class="tags spirits <?php if($spirits) echo "active" ?>">
             Spirits
-            <input id="spirits" type="checkbox"name="spirits" value="checked" />
+            <input id="spirits" type="checkbox"name="spirits" value="checked" <?php if($spirits) echo "checked" ?> />
             <span class="custom-checkbox">&chi;</span>
         </label>
-        <label class="tags venues <?php if($route === "/drinks") echo 'hide' ?>">
+        <label class="tags venues <?php if($route === "/drinks") echo 'hide'; if($venues) echo "active" ?>"> <!-- hide the venues tag if the component is used on the drinks page -->
             Venues
-            <input id="venues" type="checkbox"name="venues" value="checked" />
+            <input id="venues" type="checkbox"name="venues" value="checked" <?php if($venues) echo "checked" ?> />
             <span class="custom-checkbox">&chi;</span>
         </label>
     </div>
 </form>
 <script type="text/JavaScript" src="../JS/searchbar.js"></script>
 <?php
-    
+    // hides any $_POST related errors when used on the root URL
     if(!$_POST) {
-        $results = "";
+        $searchQuery = "";
     }
     else {
         // search page stuff - not needed for home page or venues
-        $results = $_POST["search"];
+        $searchQuery = $_POST["search"];
         include "./searchDB.php";
-        $results = search($results);
-        for($i = 0; $i < count($results); $i++) {
-            include "./views/components/result.php";
-            // echo $results[$i]->name;
-            // echo $results[$i]->percentage;
+        $results = search($searchQuery); // searches the database and returns the results to this variable
+        if(count($results) <= 0) {
+            echo "<p class='no-reults'>No results found. Try narrowing your search.</p>";
+        }
+        else {
+            // loops through each result in the array of results and displays the result component
+            for($i = 0; $i < count($results); $i++) {
+                $result = $results[$i];
+                include "./views/components/result.php";
+            }
         }
     }
-    return $results;
 ?>
